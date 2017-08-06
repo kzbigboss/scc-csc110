096c580ca6be9292b86dad37899ca2e0b17280d8
Mark Kazzaz <markkazzaz@gmail.com>
2017-08-06 15:28:40 -0700

    progress on lab 6

---
+++ module 6/KazzazLab06SecHYVer01.py
@@ -0,0 +1,98 @@
+from graphics import *
+
+def drawMark(window, intSize, lstMarkerLocations):
+    ## take the list of marker locations and draw circles
+    ## to represent the request face value of the die
+    for i in range(len(lstMarkerLocations)):
+        cirMark = Circle(Point(lstMarkerLocations[i][0], lstMarkerLocations[i][1]), intSize*0.1)
+        cirMark.setFill('green')
+        cirMark.draw(window)
+
+def drawDice(window, pntTopLeft, intSize, intFaceValue):
+    ## figure out corresponding bottom right corner based on
+    ## top left corner input
+    pntBottomRight = Point(pntTopLeft.getX()+intSize
+                           ,pntTopLeft.getY()+intSize
+                           )
+
+    ## define and draw rectangle shape based on top corner & bottom corner
+    ## into window input
+    rctDice = Rectangle(pntTopLeft, pntBottomRight)
+    rctDice.setFill('white')
+    rctDice.draw(window)
+
+    ## define offsets and partitions for dice face marker locations
+    fltXOffset          = pntTopLeft.getX()
+    fltYOffset          = pntTopLeft.getY()
+    fltNearPartition    = 0.2
+    fltMidPartition     = 0.5
+    fltFarPartition     = 0.8
+
+    ## calculate the relative location of the dice marker locations
+    ## based on the window placement and width of the dice
+
+    lstMarkerLocations = [  # list of a list containing dice marker locations
+        ### map of all maker points on a dice face:
+            ### --------------
+            ### | p0  p1  p2 |
+            ### |            |
+            ### | p3  p4  p5 |
+            ### |            |
+            ### | p6  p7  p8 |
+            ### --------------
+        
+        ## first row
+         [fltNearPartition*intSize+fltXOffset, fltNearPartition*intSize+fltYOffset] #p0
+        ,[fltMidPartition*intSize+fltXOffset, fltNearPartition*intSize+fltYOffset]  #p1
+        ,[fltFarPartition*intSize+fltXOffset, fltNearPartition*intSize+fltYOffset]  #p2
+        ## second row
+        ,[fltNearPartition*intSize+fltXOffset, fltMidPartition*intSize+fltYOffset]  #p3
+        ,[fltMidPartition*intSize+fltXOffset, fltMidPartition*intSize+fltYOffset]   #p4
+        ,[fltFarPartition*intSize+fltXOffset, fltMidPartition*intSize+fltYOffset]   #p5
+        ## third row
+        ,[fltNearPartition*intSize+fltXOffset, fltFarPartition*intSize+fltYOffset]  #p6
+        ,[fltMidPartition*intSize+fltXOffset, fltFarPartition*intSize+fltYOffset]   #p7
+        ,[fltFarPartition*intSize+fltXOffset, fltFarPartition*intSize+fltYOffset]   #p8
+         ]
+
+    ## evaluate the face value requested and determine
+    ## the allowable markers to draw
+    if intFaceValue == 1:
+        lstMarkIndex = [4]
+    elif intFaceValue == 2:
+        lstMarkIndex = [0,8]
+    elif intFaceValue == 3:
+        lstMarkIndex = [0,4,8]
+    elif intFaceValue == 4:
+        lstMarkIndex = [0,2,6,8]
+    elif intFaceValue == 5:
+        lstMarkIndex = [0,2,4,6,8]
+    elif intFaceValue == 6:
+        lstMarkIndex = [0,2,3,5,6,8]
+
+    ## subset the marker location listing to just those necesssary to
+    ## create the requested face value
+    lstMarkerLocations = [lstMarkerLocations[i] for i in lstMarkIndex]
+
+    ## leverage the diceMark function to draw dice markers for the
+    ## resulting dice markers based on the requested face value
+    diceMarks = drawMark(window, intSize, lstMarkerLocations)
+
+def main():
+    ## define window for GUI
+    window_width = 400
+    window_height = 400
+    win = GraphWin('Lab6', window_width, window_height)
+    win.setBackground('grey')
+
+    ## leverage the drawDice function to draw dice in the window based
+    ## on entering the the top left corner of the die, the size of the die,
+    ## and the face value of the dice
+    rctDice01 = drawDice(win, Point(50, 50), 50, 6)
+    rctDice02 = drawDice(win, Point(100, 100), 50, 5)
+    rctDice03 = drawDice(win, Point(150, 150), 50, 4)
+    rctDice04 = drawDice(win, Point(200, 200), 50, 3)
+    rctDice05 = drawDice(win, Point(250, 250), 50, 2)
+    rctDice06 = drawDice(win, Point(300, 300), 50, 1)
+
+main()
